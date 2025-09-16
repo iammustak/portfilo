b896ab70ce425224269d45a4a1e7811939e16b78
Md Mustak Alam <mdmustakalam874@gmail.com>
2025-09-16 16:56:19 +0530

    fixing

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  base: './', // ✅ ensures assets work on any deployment path
+  plugins: [tailwindcss(), react()],
 })
